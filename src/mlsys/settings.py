f2caeeed2185eeb194274a8fe5b356f5d06501f5
sfnsys710 <soufianenidam7@gmail.com>
2025-11-02 16:39:59 +0100

    fix pytest

--- src/mlsys/settings.py
+++ src/mlsys/settings.py
@@ -12,6 +12,6 @@ GCP_PROJECT_ID = os.getenv("GCP_PROJECT_ID")
 GCP_REGION = os.getenv("GCP_REGION")
 
 # GCS Model Buckets
-GCS_BUCKET_MODELS_DEV = os.getenv("GCS_BUCKET_MODELS_DEV", "ml-models-dev")
-GCS_BUCKET_MODELS_STAGING = os.getenv("GCS_BUCKET_MODELS_STAGING", "ml-models-staging")
-GCS_BUCKET_MODELS_PROD = os.getenv("GCS_BUCKET_MODELS_PROD", "ml-models-prod")
+GCS_BUCKET_MODELS_DEV = os.getenv("GCS_BUCKET_MODELS_DEV", "mlsys-models-dev")
+GCS_BUCKET_MODELS_STAGING = os.getenv("GCS_BUCKET_MODELS_STAGING", "mlsys-models-staging")
+GCS_BUCKET_MODELS_PROD = os.getenv("GCS_BUCKET_MODELS_PROD", "mlsys-models-prod")
